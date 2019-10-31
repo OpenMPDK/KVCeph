62222a16d8f9bcc2b391dbd565de80f229774338
Pratik Mishra <mishra.p@samsung.com>
2019-10-31 11:02:55 -0700

    Scripts from Francisco: run_kvsstore.sh

--- build/run_kvsstore.sh
+++ build/run_kvsstore.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-devicenum=2 # Has to match the vstart_kvs.sh
+devicenum=$(grep nvme ../src/vstart_kvs.sh | awk '{print $2}' | cut -d '"' -f 2)
+echo "$devicenum specified in vstart_kvs.sh"
+
 
 echo "Stop any running ceph processes"
 sudo ../src/stop.sh
 
-echo "Remove previous local directories"
+echo "Remove existing local directories"
 sudo rm -rf keyring
 sudo rm -rf out
 sudo rm -rf dev
 
 
 echo "Format device $devicenum"
-sudo nvme format /dev/nvme${devicenum}n1 -s0 -n1
+sudo nvme format $devicenum -s0 -n1
 echo "## VSTART"
-echo "sudo CEPH_DEV=1 MON=1 MDS=0 MGR=1 RGW=0 OSD=1 ../src/vstart_kvs.sh -n -X --kvsstore"
-sudo CEPH_DEV=1 MON=1 MDS=0 MGR=1 RGW=0 OSD=1 ../src/vstart_kvs.sh -n -X --kvsstore
+echo "sudo CEPH_DEV=1 MON=1 MDS=1 MGR=1 RGW=1 OSD=1 ../src/vstart_kvs.sh -n -X --kvsstore"
+sudo CEPH_DEV=1 MON=1 MDS=1 MGR=1 RGW=1 OSD=1 ../src/vstart_kvs.sh -n -X --kvsstore
