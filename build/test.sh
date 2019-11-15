abb117a6275e5cf6cc8ae1878011fd521aa4a71f
franc <f.londono@partner.samsung.com>
2019-11-15 14:52:49 -0800

    added test.sh

---
+++ build/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo CEPH_DEV=1 ./bin/ceph osd pool create rbd 100
+sudo CEPH_DEV=1 ./bin/ceph osd pool application enable rbd cephfs
+# size: # of replicas
+sudo CEPH_DEV=1 ./bin/ceph osd pool set rbd size 1
+# min_size: # of replicas to be written before sending an ACK to a client 
+sudo CEPH_DEV=1 ./bin/ceph osd pool set rbd min_size 1 
+
+sudo ./bin/rados bench -p rbd -b 4096 --max-objects 100000 --run-name 1 -t 64 3600 write --no-cleanup
+echo "sudo ./bin/rados bench -p rbd --max-objects 100000 --run-name 1 -t 64 30 rand"
+echo "sudo ./bin/rados bench -p rbd --max-objects 100000 --run-name 1 -t 64 30 seq"
+
