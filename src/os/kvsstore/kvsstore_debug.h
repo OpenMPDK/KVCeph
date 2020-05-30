7b04c6fda28a0aba25ed627d3d962a6ef2b1f945
Pratik Mishra <mishra.p@samsung.com>
2020-05-30 01:54:10 -0700

    Disable debug printing for cleanup

--- src/os/kvsstore/kvsstore_debug.h
+++ src/os/kvsstore/kvsstore_debug.h
@@ -3,7 +3,7 @@
 
 #define _KVSFILELOGGING
 #include "kvsstore_debug_impl.h"
-#define ENABLE_FTRACE
+//#define ENABLE_FTRACE
 
 // common
 #define _KVSLOGGER_IMPL(a,b) SimpleLoggerBuffer() << "[" << get_thread_name() << "," << std::setw(15) << get_fixed_func_name(a) << "," << std::setw(5) << b << "] " 
